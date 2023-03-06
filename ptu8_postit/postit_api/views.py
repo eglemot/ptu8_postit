7bf09c68127ecacb14042f4951fb509de972b39c
Kestutis Januskevicius <infohata@gmail.com>
2023-03-06 13:30:19 +0200

    CRUD for post with permissions

--- ptu8_postit/postit_api/views.py
+++ ptu8_postit/postit_api/views.py
@@ -1,4 +1,6 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 from . import models, serializers
 
 
@@ -9,3 +11,23 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.PostSerializer
+    queryset = models.Post.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.queryset.filter(pk=kwargs['pk'], user=self.request.user)
+        if obj.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('Object not found or does not belong to you.'))
+
+    def put(self, request, *args, **kwargs):
+        obj = self.queryset.filter(pk=kwargs['pk'], user=self.request.user)
+        if obj.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('Object not found or does not belong to you.'))
