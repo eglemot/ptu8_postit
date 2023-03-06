be40c9d2162a4cf8b1eee0e1679f6dda024d2335
Kestutis Januskevicius <infohata@gmail.com>
2023-03-06 14:05:39 +0200

    abstract Update Delete mixin

--- ptu8_postit/postit_api/views.py
+++ ptu8_postit/postit_api/views.py
@@ -4,20 +4,7 @@ from rest_framework.exceptions import ValidationError
 from . import models, serializers
 
 
-class PostList(generics.ListCreateAPIView):
-    serializer_class = serializers.PostSerializer
-    queryset = models.Post.objects.all()
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = serializers.PostSerializer
-    queryset = models.Post.objects.all()
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
-
+class UserOwnedObjectRUDMixin():
     def delete(self, request, *args, **kwargs):
         obj = self.queryset.filter(pk=kwargs['pk'], user=self.request.user)
         if obj.exists():
@@ -31,6 +18,21 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
             return self.update(request, *args, **kwargs)
         else:
             raise ValidationError(_('Object not found or does not belong to you.'))
+
+
+class PostList(generics.ListCreateAPIView):
+    serializer_class = serializers.PostSerializer
+    queryset = models.Post.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, UserOwnedObjectRUDMixin):
+    serializer_class = serializers.PostSerializer
+    queryset = models.Post.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -50,21 +52,7 @@ class CommentList(generics.ListCreateAPIView):
         return qs
 
 
-class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView, UserOwnedObjectRUDMixin):
     serializer_class = serializers.CommentSerializer
     queryset = models.Comment.objects.all()
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
-
-    def delete(self, request, *args, **kwargs):
-        obj = self.queryset.filter(pk=kwargs['pk'], user=self.request.user)
-        if obj.exists():
-            return self.destroy(request, *args, **kwargs)
-        else:
-            raise ValidationError(_('Object not found or does not belong to you.'))
-
-    def put(self, request, *args, **kwargs):
-        obj = self.queryset.filter(pk=kwargs['pk'], user=self.request.user)
-        if obj.exists():
-            return self.update(request, *args, **kwargs)
-        else:
-            raise ValidationError(_('Object not found or does not belong to you.'))
