418f459c50d500ba7b88a57c69e43fd7763f6811
Kestutis Januskevicius <infohata@gmail.com>
2023-03-06 09:42:49 +0200

    Create view

--- ptu8_postit/postit_api/views.py
+++ ptu8_postit/postit_api/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 from . import models, serializers
 
 
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     serializer_class = serializers.PostSerializer
     queryset = models.Post.objects.all()
