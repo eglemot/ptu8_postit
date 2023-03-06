7893f73960fb7ccaa32b45c59ba79d40d2888ee2
Kestutis Januskevicius <infohata@gmail.com>
2023-03-06 09:59:37 +0200

    user relationship in API, part 0

--- ptu8_postit/postit_api/serializers.py
+++ ptu8_postit/postit_api/serializers.py
@@ -3,6 +3,9 @@ from . import models
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = models.Post
-        fields = ('id', 'title', 'body', 'user', 'created')
+        fields = ('id', 'title', 'body', 'user', 'user_id', 'created')
--- ptu8_postit/postit_api/views.py
+++ ptu8_postit/postit_api/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from . import models, serializers
 
 
 class PostList(generics.ListCreateAPIView):
     serializer_class = serializers.PostSerializer
     queryset = models.Post.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
