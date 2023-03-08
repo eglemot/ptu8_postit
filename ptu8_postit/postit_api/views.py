a676db1dd7b719934cabfc98fee3f52b4777c5ba
Kestutis Januskevicius <infohata@gmail.com>
2023-03-08 08:59:56 +0200

    added delete method to user

--- ptu8_postit/postit_api/views.py
+++ ptu8_postit/postit_api/views.py
@@ -9,10 +9,18 @@ from . import models, serializers
 User = get_user_model()
 
 
-class UserCreate(generics.CreateAPIView):
+class UserCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = (permissions.AllowAny, )
+
+    def delete(self, request, *args, **kwargs):
+        user = User.objects.filter(pk=self.request.user.pk)
+        if user.exists():
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('User does not exist.')
 
 
 class UserOwnedObjectRUDMixin():
